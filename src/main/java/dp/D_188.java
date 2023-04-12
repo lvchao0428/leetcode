5044fe7490d8601a47e209373cc4ab731a512b43
charlielv <charlielv@tencent.com>
2023-04-12 22:53:30 +0800

    update dp 188

--- src/main/java/dp/D_188.java
+++ src/main/java/dp/D_188.java
@@ -1,32 +1,32 @@
 package dp;
 
 public class D_188 {
-  public int maxProfit(int k, int[] prices) {
+  public int maxProfit(int maxK, int[] prices) {
     int n = prices.length;
-    if (n <= 0) {
+    if (n == 0) {
       return 0;
     }
-    if (k > n/2) {
+    if (maxK > n/2) {
       return maxNokProfit(prices);
     }
-    int[][][] dp = new int[n][k+1][2];
+    int[][][] dp = new int[n][maxK+1][2];
     for (int i = 0; i < n; ++i) {
       dp[i][0][1] = Integer.MIN_VALUE;
       dp[i][0][0] = 0;
     }
     for (int i = 0; i < n; ++i) {
-      for (int j = k; j >= 1; --j) {
+      for (int k = maxK; k >=1 ; --k) {
         if (i-1 == -1) {
-          dp[i][j][0] = 0;
-          dp[i][j][1] = -prices[i];
+          dp[i][k][0] = 0;
+          dp[i][k][1] = -prices[i];
           continue;
         }
-        dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
-        dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+        dp[i][k][0] = Math.max(dp[i-1][k][0], dp[i-1][k][1] + prices[i]);
+        dp[i][k][1] = Math.max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i]);
       }
     }
 
-    return dp[n-1][k][0];
+    return dp[n-1][maxK][0];
   }
 
   public int maxNokProfit(int[] prices) {
