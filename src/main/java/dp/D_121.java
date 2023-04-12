b9676c3bf90e700324519ac42f5f1e66ff1ca1b7
charlielv <charlielv@tencent.com>
2023-04-12 17:36:06 +0800

    update

---
+++ src/main/java/dp/D_121.java
@@ -0,0 +1,18 @@
+package dp;
+
+public class D_121 {
+  public int maxProfit(int[] prices) {
+    int n = prices.length;
+    int[][] dp = new int[n][2];
+    for (int i = 0; i < n; ++i) {
+      if (i-1 == -1) {
+        dp[i][0] = 0;
+        dp[i][1] = -prices[i];
+      } else {
+        dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+        dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+      }
+    }
+    return dp[n-1][0];
+  }
+}
