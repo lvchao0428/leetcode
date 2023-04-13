2aa1f3a1ec3d8b5d4d802ba76dea2fdb59ea00b9
charlielv <charlielv@tencent.com>
2023-04-13 16:16:34 +0800

    add dp 139

---
+++ src/main/java/dp/D_139.java
@@ -0,0 +1,38 @@
+package dp;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class D_139 {
+  int[] memo;
+  public boolean wordBreak(String s, List<String> wordDict) {
+    memo = new int[s.length()];
+    Arrays.fill(memo, -1);
+    return dp(s, 0, wordDict);
+  }
+
+  public boolean dp(String s, int i, List<String> wordDict) {
+    if (i == s.length()) {
+      return true;
+    }
+    if (memo[i] != -1) {
+      return memo[i] == 1 ? true : false;
+    }
+    for (String word : wordDict) {
+      int len = word.length();
+      if (i + len > s.length()) {
+        continue;
+      }
+      String subStr = s.substring(i, i + len);
+      if (!word.equals(subStr)) {
+        continue;
+      }
+      if (dp(s, i + len, wordDict)) {
+        memo[i] = 1;
+        return true;
+      }
+    }
+    memo[i] = 0;
+    return false;
+  }
+}
